cef046d252b4241dee7a0585f5ae1ac3a79a216e
Andrey Kozlov <andreydmitrievich666@gmail.com>
2018-04-01 22:30:25 +0300

    step 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NoteListComponent } from './note-list/note-list.component';
+import { SettingsComponent } from './settings/settings.component';
+import { NoteEditorComponent } from './note-editor/note-editor.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'notes' },
+  { path: 'notes', component: NoteListComponent },
+  { path: 'notes/edit/:id', component: NoteEditorComponent },
+  { path: 'settings', component: SettingsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { NoteListComponent } from './note-list/note-list.component';
+import { SettingsComponent } from './settings/settings.component';
+import { NoteService } from './shared/note.service';
+import { NoteEditorComponent } from './note-editor/note-editor.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoteListComponent,
+    SettingsComponent,
+    NoteEditorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    NoteService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
